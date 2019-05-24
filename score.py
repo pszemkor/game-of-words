b2fd39a6a235ea0516d0b4788d0bf2269705c041
okarinbro <prz.jab98@gmail.com>
2019-05-24 13:49:21 +0200

    code reformatted + score counter improved

--- score.py
+++ score.py
@@ -8,6 +8,7 @@ class ScoreCounter:
         self.newly_added = newly_added
         self.tiles_with_fixed_neighbours = tiles_with_fixed_neighbours
 
+
     def __count_word_bonus(self, pos):
         factor = 1
         if self.board.fields[pos[0]][pos[1]].bonus == model.Bonus.BONUS_2W:
@@ -50,6 +51,20 @@ class ScoreCounter:
                 tiles_with_fixed_neighbour.append(pos)
         return tiles_with_fixed_neighbour
 
+    def __check_whether_has_vertical_neighbours(self, pos):
+        if pos[0] + 1 < config.BOARD_SIZE and self.board.fields[pos[0]+1][pos[1]].state == model.FieldState.FIXED:
+            return True
+        if pos[0] - 1 >= 0 and self.board.fields[pos[0]-1][pos[1]].state == model.FieldState.FIXED:
+            return True
+        return False
+
+    def __check_whether_has_horizontal_neighbours(self, pos):
+        if pos[1] + 1 < config.BOARD_SIZE and self.board.fields[pos[0]][pos[1]+1].state == model.FieldState.FIXED:
+            return True
+        if pos[1] - 1 >= 0 and self.board.fields[pos[0]][pos[1]-1].state == model.FieldState.FIXED:
+            return True
+        return False
+
     def horizontal_word_score(self, pos):
         score = 0
         if pos[1] + 1 < config.BOARD_SIZE and self.board.fields[pos[0]][pos[1] + 1].state == model.FieldState.FIXED:
@@ -76,7 +91,7 @@ class ScoreCounter:
                 score += self.board.fields[x][pos[1]].tile.get_value()
                 print("V coords", x, pos[1])
 
-        if pos[0] - 1 >= 0 and self.board.fields[pos[0]-1][pos[1]].state == model.FieldState.FIXED:
+        if pos[0] - 1 >= 0 and self.board.fields[pos[0] - 1][pos[1]].state == model.FieldState.FIXED:
             for x in range(pos[0] - 1, -1, -1):
                 if self.board.fields[x][pos[1]].state == model.FieldState.EMPTY:
                     break
@@ -88,8 +103,8 @@ class ScoreCounter:
 
         if self.newly_added is None:
             self.newly_added = self.__get_newly_added()
-            print("BLAAAAAAAAH")
             print(self.newly_added)
+            # for ai pass
             if len(self.newly_added) == 0:
                 return 0
         if self.tiles_with_fixed_neighbours is None:
@@ -98,27 +113,32 @@ class ScoreCounter:
         score = 0
         horizontal_sorted = sorted(self.newly_added, key=lambda x: x[1])
         vertical_sorted = sorted(self.newly_added, key=lambda x: x[0])
-        if len(horizontal_sorted) == 1 or horizontal_sorted[0][0] == horizontal_sorted[1][0]:
+        if (len(horizontal_sorted) == 1 and self.__check_whether_has_horizontal_neighbours(horizontal_sorted[0])) \
+                or horizontal_sorted[0][0] == horizontal_sorted[1][0]:
             # horizontal word which consists of newly-added letters (with temporary state)
             word_bonus_factor = 1
             x = horizontal_sorted[0][0]
             for y in range(horizontal_sorted[0][1], -1, -1):
                 if self.board.fields[x][y].state == model.FieldState.FIXED:
-                    print("for ", x, y, " score: ", self.board.fields[x][y].tile.get_value(), score)
                     score += self.board.fields[x][y].tile.get_value()
+                    print("for ", x, y, " value: ", self.board.fields[x][y].tile.get_value(), " score: ", score)
                 elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
-                    print("for ", x, y, " score: ",
-                          self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), score)
                     score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                    print("for ", x, y, " value: ",
+                          self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                          score)
                 else:
                     break
             if len(self.newly_added) > 1:
                 for y in range(horizontal_sorted[1][1], horizontal_sorted[::-1][0][1]):
                     if self.board.fields[x][y].state == model.FieldState.FIXED:
                         score += self.board.fields[x][y].tile.get_value()
-                        print("for ", x, y, " score: ", self.board.fields[x][y].tile.get_value(), score)
+                        print("for ", x, y, " value: ", self.board.fields[x][y].tile.get_value(), " score: ", score)
                     elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
                         score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                        print("for ", x, y, " value: ",
+                              self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                              score)
                     else:
                         break
             for y in range(horizontal_sorted[::-1][0][1], config.BOARD_SIZE):
@@ -127,9 +147,15 @@ class ScoreCounter:
                     print("for ", x, y, " score: ", self.board.fields[x][y].tile.get_value(), score)
                 elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
                     score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                    print("for ", x, y, " value: ",
+                          self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                          score)
                 else:
                     break
-            print("score: ", score)
+            if len(self.newly_added) == 1:
+                score -= self.board.fields[x][horizontal_sorted[::-1][0][1]].tile.get_value() * \
+                         self.__count_letter_bonus((x, horizontal_sorted[::-1][0][1]))
+            print("score after checking in temporary tiles line: ", score)
             for pos in horizontal_sorted:
                 word_bonus_factor *= self.__count_word_bonus(pos)
                 print("GOT BONUS FOR WORD: ", word_bonus_factor)
@@ -138,43 +164,63 @@ class ScoreCounter:
             print("after word bonus: ", score)
 
             for pos in self.tiles_with_fixed_neighbours:
-                print("counting extra vertical for letter: ", self.board.fields[pos[0]][pos[1]].tile.character, pos)
-                score += self.vertical_word_score(pos) + self.board.fields[pos[0]][pos[1]].tile.get_value()
-                score *= self.__count_word_bonus(pos)
-                print("got: ", self.vertical_word_score(pos))
+                if self.__check_whether_has_vertical_neighbours(pos):
+                    print("counting extra vertical for letter: ", self.board.fields[pos[0]][pos[1]].tile.character, pos)
+                    score += self.vertical_word_score(pos) + self.board.fields[pos[0]][pos[1]].tile.get_value()
+                    score *= self.__count_word_bonus(pos)
+                    print("got: ", self.vertical_word_score(pos))
 
         else:
             word_bonus_factor = 1
             y = vertical_sorted[0][1]
             for x in range(vertical_sorted[0][0], -1, -1):
                 if self.board.fields[x][y].state == model.FieldState.FIXED:
+                    print("for ", x, y, " value: ", self.board.fields[x][y].tile.get_value(), " score: ", score)
                     score += self.board.fields[x][y].tile.get_value()
                 elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
                     score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                    print("for ", x, y, " value: ",
+                          self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                          score)
                 else:
                     break
             if len(self.newly_added) > 1:
                 for x in range(vertical_sorted[1][0], vertical_sorted[::-1][0][0]):
                     if self.board.fields[x][y].state == model.FieldState.FIXED:
                         score += self.board.fields[x][y].tile.get_value()
+                        print("for ", x, y, " value: ", self.board.fields[x][y].tile.get_value(), " score: ", score)
                     elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
                         score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                        print("for ", x, y, " value: ",
+                              self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                              score)
                     else:
                         break
             for x in range(vertical_sorted[::-1][0][0], config.BOARD_SIZE):
                 if self.board.fields[x][y].state == model.FieldState.FIXED:
                     score += self.board.fields[x][y].tile.get_value()
+                    print("for ", x, y, " value: ", self.board.fields[x][y].tile.get_value(), " score: ", score)
                 elif self.board.fields[x][y].state == model.FieldState.TEMPORARY:
                     score += self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y))
+                    print("for ", x, y, " value: ",
+                          self.board.fields[x][y].tile.get_value() * self.__count_letter_bonus((x, y)), " score: ",
+                          score)
                 else:
                     break
-
+            if len(self.newly_added) == 1:
+                score -= self.board.fields[vertical_sorted[::-1][0][0]][y].tile.get_value() * \
+                         self.__count_letter_bonus((vertical_sorted[::-1][0][0], y))
+            print("score after checking in temporary tiles line: ", score)
             for pos in vertical_sorted:
                 word_bonus_factor *= self.__count_word_bonus(pos)
-
+                print("GOT BONUS FOR WORD: ", word_bonus_factor)
             score *= word_bonus_factor
+            print("after word bonus: ", score)
 
             for pos in self.tiles_with_fixed_neighbours:
-                score += self.horizontal_word_score(pos) + self.board.fields[pos[0]][pos[1]].tile.get_value()
-                score *= self.__count_word_bonus(pos)
+                if self.__check_whether_has_horizontal_neighbours(pos):
+                    print("counting extra vertical for letter: ", self.board.fields[pos[0]][pos[1]].tile.character, pos)
+                    score += self.horizontal_word_score(pos) + self.board.fields[pos[0]][pos[1]].tile.get_value()
+                    score *= self.__count_word_bonus(pos)
+                    print("got: ", self.vertical_word_score(pos), "curr score: ", score)
         return score
